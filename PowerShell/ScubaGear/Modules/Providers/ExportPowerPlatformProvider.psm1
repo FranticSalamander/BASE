6848a649f1e6e1b75d4478420f6de8dafcd8403e
fs18lb <fs18lb@ihub.gov.au>
2023-12-18 15:46:53 +1100

    Change powerplatform JSONs

--- PowerShell/ScubaGear/Modules/Providers/ExportPowerPlatformProvider.psm1
+++ PowerShell/ScubaGear/Modules/Providers/ExportPowerPlatformProvider.psm1
@@ -191,17 +191,17 @@ $($_)
     $PowerPlatformSuccessfulCommands = ConvertTo-Json @($Tracker.GetSuccessfulCommands())
     $PowerPlatformUnSuccessfulCommands = ConvertTo-Json @($Tracker.GetUnSuccessfulCommands())
 
-    $TenantID | Out-File -FilePath .\configs-json\pp\tenant_id_config.json
-    $EnvironmentCreation | Out-File -FilePath .\configs-json\pp\environment_creation_config.json
-    $DLPPolicies | Out-File -FilePath .\configs-json\pp\dlp_policies_config.json
-    $TenantIsolation | Out-File -FilePath .\configs-json\pp\tenant_isolation_config.json
-    $EnvironmentList | Out-File -FilePath .\configs-json\pp\environment_list_config.json
+    #$TenantID | Out-File -FilePath .\configs-json\pp\tenant_id_config.json
+    #$EnvironmentCreation | Out-File -FilePath .\configs-json\pp\environment_creation_config.json
+    #$DLPPolicies | Out-File -FilePath .\configs-json\pp\dlp_policies_config.json
+    #$TenantIsolation | Out-File -FilePath .\configs-json\pp\tenant_isolation_config.json
+    #$EnvironmentList | Out-File -FilePath .\configs-json\pp\environment_list_config.json
 
-    #$TenantID = Get-Content -Path .\configs-json\pp\tenant_id_config.json -Raw
-    #$EnvironmentCreation = Get-Content -Path .\configs-json\pp\environment_creation_config.json -Raw
-    #$DLPPolicies = Get-Content -Path .\configs-json\pp\dlp_policies_config.json -Raw
-    #$TenantIsolation = Get-Content -Path .\configs-json\pp\tenant_isolation_config.json -Raw
-    #$EnvironmentList = Get-Content -Path .\configs-json\pp\environment_list_config.json -Raw
+    $TenantID = Get-Content -Path .\configs-json\pp\tenant_id_config.json -Raw
+    $EnvironmentCreation = Get-Content -Path .\configs-json\pp\environment_creation_config.json -Raw
+    $DLPPolicies = Get-Content -Path .\configs-json\pp\dlp_policies_config.json -Raw
+    $TenantIsolation = Get-Content -Path .\configs-json\pp\tenant_isolation_config.json -Raw
+    $EnvironmentList = Get-Content -Path .\configs-json\pp\environment_list_config.json -Raw
 
     # tenant_id added for testing purposes
     # Note the spacing and the last comma in the json is important
